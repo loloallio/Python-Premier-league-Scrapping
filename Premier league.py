530bc20beed156cfadac819832661e992afeeb00
loloallio <113385427+loloallio@users.noreply.github.com>
2023-06-03 17:50:22 -0300

    Update Premier league.py

--- Premier league.py
+++ Premier league.py
@@ -9,9 +9,8 @@ import re
 import time
 
 
-
 # Open main-link
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))  # , #options=chrome_options)
 
 driver.get("https://www.premierleague.com/results")
 time.sleep(1)
@@ -31,7 +30,7 @@ pgdown = driver.find_element(By.XPATH, "/html/body/footer[@class='mainFooter']/d
 pgdown.send_keys(Keys.END)
 pgdown.send_keys(Keys.PAGE_UP)
 pgdown.send_keys(Keys.END)
-time.sleep(5)
+time.sleep(6)
 pgsour = driver.page_source
 
 pattern = re.findall(r'www.premierleague.com/match/\d*', pgsour)
@@ -39,6 +38,26 @@ match = []
 # Web sometimes has duplicates, thus this step is needed
 [match.append(x) for x in pattern if x not in match]
 
-with open('links.txt', 'w', encoding='utf-8') as file:
+with open('links1.txt', 'w', encoding='utf-8') as file1, \
+        open('links2.txt', 'w', encoding='utf-8') as file2, \
+        open('links3.txt', 'w', encoding='utf-8') as file3, \
+        open('links4.txt', 'w', encoding='utf-8') as file4, \
+        open('links5.txt', 'w', encoding='utf-8') as file5:
+    counter = 0
     for i in match:
-        file.write(f'{i}\n')
+        if counter == 0:
+            file1.write(f"{i}\n")
+            counter += 1
+        elif counter == 1:
+            file2.write(f"{i}\n")
+            counter += 1
+        elif counter == 2:
+            file3.write(f"{i}\n")
+            counter += 1
+        elif counter == 3:
+            file4.write(f"{i}\n")
+            counter += 1
+        elif counter == 4:
+            file5.write(f"{i}\n")
+            counter = 0
+
